0a159896c1152eed0be6f24cb1f66a6f3d2dca32
Lucas Visser <lucasnvp@gmail.com>
2017-09-25 11:46:05 -0300

    FileSystem - Cambios menores en el cpfrom.

--- ProcesoFileSystem/src/comandos/comandos.c
+++ ProcesoFileSystem/src/comandos/comandos.c
@@ -26,20 +26,22 @@ void copyFromFStoYamafs(char* pathFS, char* dirYamafs){
 		uint32_t cantBloques = 0;
 		char* datosLeidos = malloc(TAMANIO_FIJO_BLOQUE);
 
-		// Size del archivo
+		//Abro el archivo
 		file = fopen(pathFS, "r");
+
+		// Size del archivo
 		fseek(file, 0L, SEEK_END);
 		uint32_t sizeFile = ftell(file);
 		rewind(file);
 		printf("Tamanio del archivo a copiar: %i \n", sizeFile);
 
 		// Cantidad de bloques necesarios para el archivo
-		cantBloques = 1 + ( (sizeFile - 1) / 1000);
+		cantBloques = 1 + ( (sizeFile - 1) / TAMANIO_FIJO_BLOQUE);
 		printf("\n Cant de Bloques: %i \n", cantBloques);
 
 		// Preguntar si tengo la cantidad de espacios necesarios en los nodos //
 
-		while(fgets(datosLeidos, 200, file)){
+		while(fgets(datosLeidos, TAMANIO_FIJO_BLOQUE, file)){
 			printf("%s", datosLeidos);
 			//Enviar los datos a los bloques //
 		}
